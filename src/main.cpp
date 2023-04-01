8e83823478eb095e1ebfa8d13bbced7d853acf38
German (forodie) <german.utkingu@gmail.com>
2023-04-01 03:24:50 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "Automata.h"
+
+int main() {
+    Automata a;
+    a.on();
+    a.coin(10);
+    a.getMenu();
+    a.choice(2);
+    a.check();
+    a.cook();
+    a.finish();
+    
+    return 0;
+}
